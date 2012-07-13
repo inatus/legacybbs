d52feac87e4aca61bf9d293084cb160721e18c56
Hiroki Inagaki <hiroki@inagaki.in>
2012-07-13 19:57:01 +0900

    Upload material

---
+++ src/test/java/in/inagaki/legacybbs/arquillian/drone/ArquillianDroneSample.java
@@ -0,0 +1,68 @@
+package in.inagaki.legacybbs.arquillian.drone;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.MavenImporter;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+@RunWith(Arquillian.class)
+public class ArquillianDroneSample {
+    @Drone
+    WebDriver driver;
+
+    @ArquillianResource
+    URL contextPath;
+
+    @Deployment(testable = false)
+    public static WebArchive createTestArchive() {
+	return ShrinkWrap.create(MavenImporter.class)
+		.loadEffectivePom("pom.xml").importBuildOutput()
+		.as(WebArchive.class);
+    }
+
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testUntitled() throws Exception {
+	driver.get(contextPath.toString());
+	driver.findElement(By.id("name")).clear();
+	driver.findElement(By.id("name")).sendKeys("inatus");
+	driver.findElement(By.id("email")).clear();
+	driver.findElement(By.id("email")).sendKeys("aaaaa");
+	driver.findElement(By.id("title")).clear();
+	driver.findElement(By.id("title")).sendKeys("title");
+	driver.findElement(By.name("submit")).click();
+
+	assertEquals(driver.getPageSource().contains("“à—e‚Í•K‚¸“ü—Í‚µ‚Ä‚­‚¾‚³‚¢"), true);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+	driver.quit();
+	String verificationErrorString = verificationErrors.toString();
+	if (!"".equals(verificationErrorString)) {
+	    fail(verificationErrorString);
+	}
+    }
+
+}
---
+++ src/test/java/in/inagaki/legacybbs/selenium/SeleniumSample.java
@@ -0,0 +1,49 @@
+package in.inagaki.legacybbs.selenium;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SeleniumSample {
+    private WebDriver driver;
+    private String baseUrl;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+	driver = new FirefoxDriver();
+	baseUrl = "http://localhost:8080/legacybbs/";
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testUntitled() throws Exception {
+	driver.get(baseUrl);
+	driver.findElement(By.id("name")).clear();
+	driver.findElement(By.id("name")).sendKeys("inatus");
+	driver.findElement(By.id("email")).clear();
+	driver.findElement(By.id("email")).sendKeys("aaaaa");
+	driver.findElement(By.id("title")).clear();
+	driver.findElement(By.id("title")).sendKeys("title");
+	driver.findElement(By.name("submit")).click();
+
+	assertEquals(driver.getPageSource().contains("“à—e‚Í•K‚¸“ü—Í‚µ‚Ä‚­‚¾‚³‚¢"), true);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+	driver.quit();
+	String verificationErrorString = verificationErrors.toString();
+	if (!"".equals(verificationErrorString)) {
+	    fail(verificationErrorString);
+	}
+    }
+}
